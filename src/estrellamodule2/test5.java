aaf96a3cdba6bd44865a1d8a18e15982410e237e
User <User@CRISQUEL>
2020-11-27 15:19:20 +0700

    MODULE2

---
+++ src/estrellamodule2/test5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrellamodule2;
+import java.util.*;
+/**
+ *
+ * @author User
+ */
+public class test5 {
+    
+      public static void main(String[] args) {
+    ArrayList<String> lotion= new ArrayList<String>();
+    lotion.add("nivea");
+    lotion.add("skin white");
+    lotion.add("kojic lotion");
+    lotion.add("jorjens");
+    System.out.println(lotion);
+      }
+    
+}
